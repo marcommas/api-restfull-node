7564b33e1796d1b6629e5c829a456c3d7d7e4314
marco <ti.marco@publicacoesonline.com.br>
2018-01-26 07:25:53 -0200

    requisição GET all

--- server.js
+++ server.js
@@ -42,3 +42,20 @@ app.post('/api', function(req, res){
         });
     });
 });
+
+
+//GET all
+app.get('/api', function(req, res){
+    db.open(function(err, mongoclient){
+        mongoclient.collection('postagens', function(err, collection){
+            collection.find().toArray(function(err, results){
+                if(err){
+                    res.json(err);
+                }else{
+                    res.json(results);
+                }
+                mongoclient.close();
+            });
+        });
+    });
+});

a559cd4b8c4ab7f4960d9e2c0aff7f56e9d4135e
marco <ti.marco@publicacoesonline.com.br>
2018-01-31 07:53:39 -0200

    ajustes para o upload de imagens

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     multiparty = require('connect-multiparty'),
     mongodb = require('mongodb'),
-    objectId = require('mongodb').ObjectId;
+    objectId = require('mongodb').ObjectId,
+    fs = require('fs');
 
 
 var app = express();
@@ -36,21 +37,42 @@ app.post('/api', function(req, res){
     //libera a api para qualquer dominio
     res.setHeader('Access-Control-Allow-Origin', '*');
     
-    var dados = req.body;
-    db.open(function(err, mongoclient){
-        mongoclient.collection('postagens', function(err, collection){
-            collection.insert(dados, function(err, records){
-                if(err){
-                    res.json({'status': 'erro'});
-                }else{
-                    res.json({'status': 'inclusao realizada com sucesso'});
-                }
-                mongoclient.close();
+    var date = new Date();
+    var time_stamp = date.getTime();
+    
+    var url_imagem = time_stamp + '_' + req.files.arquivo.originalFilename;
+    var path_origem = req.files.arquivo.path;
+    var path_destino = './uploads/' + url_imagem;
+    
+    
+    
+    fs.rename(path_origem, path_destino, function(err){
+        
+        if (err) {
+            res.status(500).json({error:err});
+            return;
+        }
+        
+        var dados = {
+            url_imagem: url_imagem,
+            titulo: req.body.titulo
+        }
+        
+        db.open(function(err, mongoclient){
+            mongoclient.collection('postagens', function(err, collection){
+                collection.insert(dados, function(err, records){
+                    if(err){
+                        res.json({'status': 'erro'});
+                    }else{
+                        res.json({'status': 'inclusao realizada com sucesso'});
+                    }
+                    mongoclient.close();
+                });
             });
         });
     });
+    
 });
-
 
 //GET all
 app.get('/api', function(req, res){
